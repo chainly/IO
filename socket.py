14634c6e3790099dcd7a169135d50c428e3de200
chainly <1258626769@qq.com>
2017-06-28 11:55:46 +0800

    Update socket.py
    setsockopt socket.SO_REUSEADDR

--- socket.py
+++ socket.py
@@ -26,7 +26,9 @@ sock = socket.socket(family = socket.AF_INET, # AF_UNIX(socket file), AF_INET(ip
                                                 # SOCK_RAW(raw), ...;
                                                 # proto =  usually zero
                      )
-
+# server close firstly and in timewait
+# set is so that when we cancel out we can reuse port
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('',5555))
 # python    8372                 cool    6u     sock                0,8       0t0     199688 protocol: TCP
 fd = sock.fileno() # 6
